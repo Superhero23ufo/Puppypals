3f585a5a157f994d2acc1730db61962184bc38a1
Behzad Dominic <behzad.dominic@gmail.com>
2024-02-29 15:54:41 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+import { puppyList } from './data.js'
+import { useState } from 'react'
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup);
+
+  function handleClick(puppy) {
+    console.log("puppy id: ", puppy.id);
+    setFeatPupId(puppy.id);
+  }
+  return (
+    <div className='App'>
+      {
+        puppies.map((puppy) => {
+          return <p onClick={() => handleClick(puppy)} key={puppy.id}>{puppy.name}</p>
+        })
+      }
+      {featPupId && <div>
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+      </div>}
+    </div>
+  )
+}
+
+export default App
